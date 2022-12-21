792304b42bc1ac9a417c183c3660751e755dc388
Thithic <tc.alt@protonmail.com>
2022-12-21 22:48:10 +0100

    add new client for home monitoring

---
+++ client2.py
@@ -0,0 +1,48 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def on_message(client, userdata, message):
+    str_message = str(message.payload.decode("utf-8"))
+#    print(str_message)
+    try:
+        chambre, couloir, radiateur, salon = str_message.split(" ")
+        
+        with open("chambre.txt", "a") as f:
+            f.write(f"{chambre}\n")
+            
+        with open("couloir.txt", "a") as f:
+            f.write(f"{couloir}\n")
+            
+        with open("radiateur.txt", "a") as f:
+            f.write(f"{radiateur}\n")
+            
+        with open("salon.txt", "a") as f:
+            f.write(f"{salon}\n")
+            
+    except ValueError:
+        pass
+    
+
+
+addr = "broker.hivemq.com"
+
+client = mqtt.Client(f"cliMulti{random.random()*10000:.0f}")
+
+client.on_message=on_message
+client.connect(addr)
+client.loop_start()
+
+client.subscribe("multi/temp/TC")
+
+
+while 1:
+    time.sleep(1)
+
+client.loop_stop()
+
